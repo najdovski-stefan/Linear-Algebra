e2e976146278f0a82bcade03d0314dd88d47d7e9
stefan.najdovski <stefan.najdovski@aol.com>
2025-07-14 02:02:51 +0200

    i think i got it

--- determinants.hs
+++ determinants.hs
@@ -2,16 +2,28 @@
 -- Example:
 -- -2 5
 --  3 4
--- https://immersivemath.com/ila/ch07_determinants/ch07.html
 -- Function Matrix A => value
 -- 1D: A = (a), length + sign of the vector
 -- det(A) = a_11*a_22 - a_12*a_21
 -- 2D: we have 2 columns (vectors) their determinant is the area (with sign)
--- 3D: we have 3 vectors i guess
+-- 3D: we have 3 vectors i guess we have volume (the determinant is the volume)
+-- the form is parallelepiped
+
+-- 2D
+-- For any numeric type `a`, takes a list of lists (`[[a]]`) and returns a single value (`a`).
+-- Num a => [[a]] -> a
 determinant2x2 :: Num a => [[a]] -> a
 determinant2x2 [[a, b], [c, d]] = a * d - b * c
 determinant2x2 _ = error "Input must be a 2x2 matrix."
 
+
+
+plusOne :: Num a => a -> a
+plusOne a = a + 1
+
+sumList :: Num a => [a] -> a
+sumList [] = 0
+sumList (x:xs) = x + sumList xs
 
 main :: IO ()
 -- haskell compiler looks for main as entry
@@ -22,7 +34,19 @@ main :: IO ()
 
 main = do
     putStrLn $ "Learning about determinants using Haskell"
+
+    --unreleated, learning some syntax
+    let some_num = plusOne 5
+    putStrLn $ "Plus one:" ++ show some_num
+
+    -- sum array of elements
+    let numbers = [1, 2, 3, 10]-- 1+(2+(3+(10+ sumList []))
+    let total = sumList numbers
+    putStrLn $ "SUM OF LIST: " ++ show total
+
     let matrix = [[-2, 5], [3, 4]]
     let det = determinant2x2 matrix
+
+
     putStrLn $ "Calculating the determinant of " ++ show matrix
     putStrLn $ "Result: " ++ show det
