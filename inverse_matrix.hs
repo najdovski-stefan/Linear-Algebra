0592db97c64ea667ea9792041690b2f402d864cc
stefan.najdovski <stefan.najdovski@aol.com>
2025-07-15 03:29:28 +0200

    inverse matrix in haskell first step

--- inverse_matrix.hs
+++ inverse_matrix.hs
@@ -1,4 +1,15 @@
+-- 1. find the determinant
+
+determinant3x3 :: [[Dobule]] -> Dobule -- Number instead? not sure
+determinant3x3 [[a,b,c],[d,e,f],[g,h,i]] = 
+a*(e*i - f*h) - b*(d*i - f*g) + c*(d*h - e*g)
+determinant3x3 = error "Wrong dimensions, mst be 3x3"
+
+-- 2. 
+
 main :: IO()
 
 main = do 
   putStrLn $ "Learning Inverse Matrix Operations" 
+
+
