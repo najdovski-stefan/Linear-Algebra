72a536b833eb832f4f6a8cda18010e00fb4e905c
stefan.najdovski <stefan.najdovski@aol.com>
2025-07-15 14:31:58 +0200

    step 2 is idk

--- inverse_matrix.hs
+++ inverse_matrix.hs
@@ -5,7 +5,7 @@ determinant3x3 [[a,b,c],[d,e,f],[g,h,i]] =
 a*(e*i - f*h) - b*(d*i - f*g) + c*(d*h - e*g)
 determinant3x3 = error "Wrong dimensions, mst be 3x3"
 
--- 2. 
+-- 2. find 
 
 main :: IO()
 
